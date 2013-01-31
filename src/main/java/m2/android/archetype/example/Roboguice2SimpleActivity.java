3a7500a6b89768c45e0da52e7094388fd97435d2
hyongsoon.choi <hyongsoon.choi@nhn.com>
2013-01-31 16:45:32 +0900

    roboguice add

---
+++ src/main/java/m2/android/archetype/example/Roboguice2SimpleActivity.java
@@ -0,0 +1,31 @@
+package m2.android.archetype.example;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectResource;
+import roboguice.inject.InjectView;
+import android.graphics.drawable.Drawable;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.inject.Inject;
+
+public class Roboguice2SimpleActivity extends RoboActivity {
+	
+	@InjectView(R.id.name)             			TextView name; 
+    @InjectView(R.id.thumbnail)        			ImageView thumbnail; 
+    @InjectResource(R.drawable.icon)   			Drawable icon; 
+    @InjectResource(R.string.app_name) 			String myName; 
+    @Inject                            			LocationManager loc; 
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_roboguice2_simple);
+		name.setText("Good RoboGuice");
+		thumbnail.setImageDrawable(icon);
+	}
+	
+}
